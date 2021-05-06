eddd75bf21a7725b8c33d37497dab5d624435ed9
joshuatsmith96 <joshuatsmith96@gmail.com>
2021-05-06 12:31:00 -0500

    Revert "Revert "About-Change""
    This reverts commit 3ae4a769e89b2a01a46bc5476482f8719495e12a.

--- react-portfolio-website/src/components/ArticleComponents/AboutIntro/AboutIntro.js
+++ react-portfolio-website/src/components/ArticleComponents/AboutIntro/AboutIntro.js
@@ -3,14 +3,12 @@ import './AboutIntro.css';
 
 
 const AboutIntro = (props) => {
+
+    
+
     return (
         <div className="AboutIntro">
-            <div className="AboutDivider">
-            </div>
-            <div className="AboutDividerText">
-                <h1>Joshua Smith</h1>
-                <h2>The Web Developer your company needs!</h2>
-            </div>
+            <h1>hhheellloooooo</h1>
         </div>
     )
 }
