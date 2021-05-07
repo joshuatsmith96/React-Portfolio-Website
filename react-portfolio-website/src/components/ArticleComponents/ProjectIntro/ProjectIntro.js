208633ef9b0cdb49625cc6217a0cdc0a144bf409
joshuatsmith96 <joshuatsmith96@gmail.com>
2021-05-07 17:49:25 -0500

    Added Projects Intro

---
+++ react-portfolio-website/src/components/ArticleComponents/ProjectIntro/ProjectIntro.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './ProjectIntro.css';
+
+
+const ProjectIntro = (props) => {
+
+    
+
+    return (
+        <div className="ProjectIntro">
+            <div className="projectDivider">
+            </div>
+            <div className="projectDividerText">
+                <h1>Projects</h1>
+                <p>View Projects Created By Joshua</p>
+            </div>
+        </div>
+    )
+}
+
+export default ProjectIntro;
--- react-portfolio-website/src/components/Projects/Projects.js
+++ react-portfolio-website/src/components/Projects/Projects.js
@@ -1,10 +1,11 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import ProjectIntro from '../ArticleComponents/ProjectIntro/ProjectIntro';
 
 class Projects extends Component {
     render() {
         return (
             <div>
-                <h1>Projects</h1>
+                <ProjectIntro />
             </div>
         )
     }
